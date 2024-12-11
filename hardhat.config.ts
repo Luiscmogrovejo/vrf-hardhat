e725a45ec6c5d8820640dad035beaa8e6e5ed226
luiscmogrovejo <luis.c.mogrovejo@gmail.com>
2024-12-10 21:37:18 -0500

    more chains and better config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,7 @@ import * as dotenv from "dotenv";
 dotenv.config(); // Load environment variables from .env
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -118,6 +119,9 @@ const config: HardhatUserConfig = {
   },
   ignition: {
 
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
 
