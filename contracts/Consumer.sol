626b2a30861bd14f3d0bbf20028d522787a3e753
Vinay Singh <vinay@gameon.app>
2024-12-15 19:45:12 +0530

    added verifier to the contract

--- contracts/Consumer.sol
+++ contracts/Consumer.sol
@@ -2,11 +2,21 @@
 pragma solidity ^0.8.0;
 
 interface IVRFConsumer {
-    function fulfillRandomness(uint256 requestId, bytes32[] memory outputs) external;
+    function fulfillRandomness(
+        uint256 requestId,
+        bytes32[] memory outputs,
+        bytes memory proof,
+        uint256 randomNumber
+    ) external;
 }
 
 interface IVRFOracle {
-    function requestRandomness(uint256 words) external payable returns (uint256);
+    function requestRandomness(
+        uint256 num_values,
+        uint256 fee,
+        uint256 deadline,
+        uint8 priority
+    ) external payable returns (uint256);
 }
 
 /**
@@ -21,12 +31,17 @@ contract ImprovedRandomConsumer is IVRFConsumer {
         uint256 words;
         bool fulfilled;
         bytes32[] randomness;
+        uint256 randomNumber;
     }
 
     // Mapping from request ID to Request details
     mapping(uint256 => Request) public requests;
 
-    event RandomnessRequested(uint256 indexed requestId, address indexed requester, uint256 words);
+    event RandomnessRequested(
+        uint256 indexed requestId,
+        address indexed requester,
+        uint256 words
+    );
     event RandomnessFulfilled(uint256 indexed requestId, bytes32[] randomness);
 
     /**
@@ -44,18 +59,26 @@ contract ImprovedRandomConsumer is IVRFConsumer {
      * @param words The number of random words requested.
      * @return requestId The ID of the newly created randomness request.
      */
-    function requestMyRandomness(uint256 words) external payable returns (uint256 requestId) {
+    function requestMyRandomness(
+        uint256 words
+    ) external payable returns (uint256 requestId) {
         require(words > 0, "Number of words must be greater than zero");
 
         // Request random words from the oracle
-        requestId = IVRFOracle(oracle).requestRandomness{value: msg.value}(words);
+        requestId = IVRFOracle(oracle).requestRandomness{value: msg.value}(
+            words,
+            msg.value,
+            0,
+            1
+        );
 
         // Store the request data
         requests[requestId] = Request({
             requester: msg.sender,
             words: words,
             fulfilled: false,
-            randomness: new bytes32[](0)
+            randomness: new bytes32[](0),
+            randomNumber: 0
         });
 
         emit RandomnessRequested(requestId, msg.sender, words);
@@ -66,17 +89,26 @@ contract ImprovedRandomConsumer is IVRFConsumer {
      * @dev Only the oracle can fulfill requests.
      * @param requestId The ID of the randomness request being fulfilled.
      * @param outputs An array containing the random words.
+     * @param proof The proof of randomness (not used in this contract).
+     * @param randomNumber The random number (not used in this contract).
      */
-    function fulfillRandomness(uint256 requestId, bytes32[] memory outputs) external override {
+    function fulfillRandomness(
+        uint256 requestId,
+        bytes32[] memory outputs,
+        bytes memory proof,
+        uint256 randomNumber
+    ) external override {
         require(msg.sender == oracle, "Only the oracle can fulfill randomness");
-        require(requests[requestId].requester != address(0), "Invalid request ID");
+        require(
+            requests[requestId].requester != address(0),
+            "Invalid request ID"
+        );
         require(!requests[requestId].fulfilled, "Already fulfilled");
-        require(outputs.length == requests[requestId].words, "Incorrect number of random words");
 
         // Update the request record to indicate fulfillment
         requests[requestId].fulfilled = true;
         requests[requestId].randomness = outputs;
-
+        requests[requestId].randomNumber = randomNumber;
         emit RandomnessFulfilled(requestId, outputs);
     }
 
@@ -95,7 +127,9 @@ contract ImprovedRandomConsumer is IVRFConsumer {
      * @param requestId The ID of the request to retrieve randomness for.
      * @return An array of random words.
      */
-    function getRandomness(uint256 requestId) external view returns (bytes32[] memory) {
+    function getRandomness(
+        uint256 requestId
+    ) external view returns (bytes32[] memory) {
         require(requests[requestId].fulfilled, "Randomness not received yet");
         return requests[requestId].randomness;
     }
